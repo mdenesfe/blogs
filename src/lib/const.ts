389376e036e6e6dbf8948aaca8bca94318e8c43c
Enes Demir <91777967+mdenesfe@users.noreply.github.com>
2023-11-21 18:34:44 +0300

    up

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,11 +1,11 @@
 export const SOCIAL = {
   email: "mdenesfe@gmail.com",
-  // unsplash: "https://unsplash.com/@ademilter",
-  // vsco: "https://vsco.co/adem/gallery",
-  // instagram: "https://instagram.com/ademilter",
+  unsplash: "https://unsplash.com/@mdenesfe",
+  vsco: "https://vsco.co/mdenesfe/gallery",
+  instagram: "https://instagram.com/mdenesfe",
   twitter: "https://twitter.com/mdenesfe",
   medium: "https://medium.com/@mdenesfe",
   github: "https://github.com/mdenesfe",
-  // youtube: "https://youtube.com/ademilter",
-  // superpeer: "https://superpeer.com/adem",
+  youtube: "https://youtube.com/mdenesfe",
+  superpeer: "https://superpeer.com/mdenesfe",
 };
